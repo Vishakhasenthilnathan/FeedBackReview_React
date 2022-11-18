7478b98aed2ff62f5158c30cca32f44d1a394768
VishakhaSNathan <Vishakhasenthilnathan@gmail.com>
2022-11-18 14:11:38 +0530

    Feedback app basics without backend

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Card from '../components/shared/Card'
+import {Link} from 'react-router-dom'
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a react app to leave feedback for product or services</p>
+        <p>v1.0.0</p>
+        <p>
+          <Link to='/'>Back to Home</Link>
+        </p>
+      </div>
+    </Card>
+  )
+}
+
+export default AboutPage
